10a7c803211ab5fa0f43bb6527d40792573faf76
rumgal <rumgal@gmail.com>
2022-11-14 19:37:00 +0100

    upd test back

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,68 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-// config pusher 
-const Pusher = require('pusher');
+// config pusher
+const Pusher = require("pusher");
 const pusher = new Pusher({
-  appId: process.env.PUSHER_APPID,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  cluster: process.env.PUSHER_CLUSTER,
-  useTLS: true,
+	appId: process.env.PUSHER_APPID,
+	key: process.env.PUSHER_KEY,
+	secret: process.env.PUSHER_SECRET,
+	cluster: process.env.PUSHER_CLUSTER,
+	useTLS: true,
 });
 
-const cloudinary = require('cloudinary').v2;
-const uniqid = require('uniqid');
-const fs = require('fs'); // module pour déplacer les fichier
+const cloudinary = require("cloudinary").v2;
+const uniqid = require("uniqid");
+const fs = require("fs"); // module pour déplacer les fichier
 
 var socketId = null;
 // Join chat
-router.put('/users/:username/:channel', (req, res) => {
- /*  pusher.connection.bind("connected", () => { // recupération de socketId
+router.put("/users/:username/:channel", (req, res) => {
+	/*  pusher.connection.bind("connected", () => { // recupération de socketId
   socketId = pusher.connection.socket_id;
 }); */
-  const authResponse = pusher.authorizeChannel("137467.5400872", `private-${req.params.channel}`);
-  console.log(authResponse);
-  // rejoindre un channel par la methode trigger
-  pusher.trigger(`private-${req.params.channel}`, 'join', { username: req.params.username });
+	const authResponse = pusher.authorizeChannel("137467.5400872", `private-${req.params.channel}`);
+	console.log(authResponse);
+	// rejoindre un channel par la methode trigger
+	pusher.trigger(`private-${req.params.channel}`, "join", { username: req.params.username });
 
-  res.json({ result: true, authResponse });
+	res.json({ result: true, authResponse });
 });
 
 // pusher.trigger(`private-${req.params.channel}`, 'leave', { username: req.params.username });
 // Leave chat
 router.delete("/users/:username/:channel", (req, res) => {
-  // permet de quitter le channel
-  pusher.trigger(`private-${req.params.channel}`, 'leave', { username: req.params.username });
+	// permet de quitter le channel
+	pusher.trigger(`private-${req.params.channel}`, "leave", { username: req.params.username });
 
-  res.json({ result: true });
+	res.json({ result: true });
 });
 
 // Send message
-router.post('/message/:channel', async (req, res) => {
-  const message = req.body;
-  console.log("req => ", message);
+router.post("/message/:channel", async (req, res) => {
+	const message = req.body;
+	console.log("req => ", message);
 
-  if (message.type === 'audio') {
-    const audioPath = `./tmp/${uniqid()}.m4a`; // créer un chemin 
-    const resultMove = await req.files.audio.mv(audioPath); // déplacer vers le chemin tmp et return une reponse
+	if (message.type === "audio") {
+		const audioPath = `./tmp/${uniqid()}.m4a`; // créer un chemin
+		const resultMove = await req.files.audio.mv(audioPath); // déplacer vers le chemin tmp et return une reponse
 
-    if (!resultMove) { // verifier si resultMove est vide on rentre dans le if
-      const resultCloudinary = await cloudinary.uploader.upload(audioPath, { resource_type: 'video' }); // sert a envoyer vers cloudinary
-      message.url = resultCloudinary.secure_url;
-      console.log(message);
-      fs.unlinkSync(audioPath); // sert a delete de tmp
-    } else {
-      res.json({ result: false, error: resultMove });
-      return;
-    }
-  }
+		if (!resultMove) {
+			// verifier si resultMove est vide on rentre dans le if
+			const resultCloudinary = await cloudinary.uploader.upload(audioPath, { resource_type: "video" }); // sert a envoyer vers cloudinary
+			message.url = resultCloudinary.secure_url;
+			console.log(message);
+			fs.unlinkSync(audioPath); // sert a delete de tmp
+		} else {
+			res.json({ result: false, error: resultMove });
+			return;
+		}
+	}
 
-  pusher.trigger(req.params.channel, 'message', message);
+	pusher.trigger(req.params.channel, "message", message);
 
-  res.json({ result: true });
+	res.json({ result: true });
 });
-
 
 //use JWTs here to authenticate users before continuing
 
@@ -89,7 +89,6 @@ router.post('/message/:channel', async (req, res) => {
       console.error(error)
   }
 } */
-
 
 // Pour la presence
 
@@ -120,13 +119,6 @@ router.post('/message/:channel', async (req, res) => {
   res.send(authResponse);
 }); */
 
-
-
-
-
-
-
-
 /* app.post("/pusher/auth", (req, res) => {
   const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
@@ -134,8 +126,9 @@ router.post('/message/:channel', async (req, res) => {
   res.send(authResponse);
 }); */
 
-
-
-
+// test backend
+router.get("/test", function (req, res) {
+	res.json({ result: true, error: "backend off" });
+});
 
 module.exports = router;
